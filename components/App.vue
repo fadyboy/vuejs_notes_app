91760a8732434a0ed43b098e2b9daad64c8420d8
aofadero <aofadero@gmail.com>
2017-09-24 01:17:48 -0400

    further updates

---
+++ components/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h1>Helo</h1>
+      <!-- <toolbar></toolbar>
+      <notes-list></notes-list>
+      <editor></editor> -->
+  </div>
+</template>
+
+<script>
+    import Toolbar from './Toolbar.vue'
+    import NotesList from './NotesList.vue'
+    import Editor from './Editor.vue'
+
+    export default {
+        components: {
+            Toolbar,
+            NotesList,
+            Editor
+        }
+    }
+
+</script>
--- vuex/store.js
+++ vuex/store.js
@@ -13,6 +13,32 @@ const state = {
 // define possible mutations for state
 const mutations = {
 
+    ADD_NOTE(state){
+        const newNote = {
+            text: 'New Note',
+            favorite: false
+        }
+        state.notes.push(newNote)
+        state.activeNote = newNote
+    },
+
+    EDIT_NOTE(state, text){
+        state.activeNote.text = text
+    },
+
+    DELETE_NOTE(state) {
+        state.notes.$remove(state.activeNote)
+        state.activeNote = state.notes[0]
+    },
+
+    TOGGLE_FAVORITE(state){
+        state.activeNote.favorite = !state.activeNote.favorite
+    },
+
+    SET_ACTIVE_NOTE(state, note){
+        state.activeNote = note
+    }
+
 }
 
 // create Vuex instance by combining state and mutations objects and export to Vuex store for use by components
